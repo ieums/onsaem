5996ab8072d2d50b4b536cdf322578a974d38e5a
ryurujxx <251049863+ryurujxx@users.noreply.github.com>
2026-05-22 02:38:13 +0900

    fix: 아고라 토큰 생성 공식 코드로 교체 (AccessToken2, RtcTokenBuilder2, ByteBuf, Utils)

--- backend/src/main/java/com/ieum/backend/global/agora/AccessToken2.java
+++ backend/src/main/java/com/ieum/backend/global/agora/AccessToken2.java
@@ -1,176 +1,373 @@
 package com.ieum.backend.global.agora;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-import java.io.ByteArrayOutputStream;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.zip.Deflater;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 
 public class AccessToken2 {
-    public static final int VERSION = 7;
-    public static final String VERSION_STR = "007";
+    public enum PrivilegeRtc {
+        PRIVILEGE_JOIN_CHANNEL(1), PRIVILEGE_PUBLISH_AUDIO_STREAM(2), PRIVILEGE_PUBLISH_VIDEO_STREAM(3), PRIVILEGE_PUBLISH_DATA_STREAM(4),;
 
-    public String appId;
-    public String appCertificate;
+        public short intValue;
+
+        PrivilegeRtc(int value) {
+            intValue = (short) value;
+        }
+    }
+
+    public enum PrivilegeRtm {
+        PRIVILEGE_LOGIN(1),;
+
+        public short intValue;
+
+        PrivilegeRtm(int value) {
+            intValue = (short) value;
+        }
+    }
+
+    public enum PrivilegeFpa {
+        PRIVILEGE_LOGIN(1),;
+
+        public short intValue;
+
+        PrivilegeFpa(int value) {
+            intValue = (short) value;
+        }
+    }
+
+    public enum PrivilegeChat {
+        PRIVILEGE_CHAT_USER(1), PRIVILEGE_CHAT_APP(2),;
+
+        public short intValue;
+
+        PrivilegeChat(int value) {
+            intValue = (short) value;
+        }
+    }
+
+    public enum PrivilegeApaas {
+        PRIVILEGE_ROOM_USER(1), PRIVILEGE_USER(2), PRIVILEGE_APP(3),;
+
+        public short intValue;
+
+        PrivilegeApaas(int value) {
+            intValue = (short) value;
+        }
+    }
+
+    private static final String VERSION = "007";
+    public static final short SERVICE_TYPE_RTC = 1;
+    public static final short SERVICE_TYPE_RTM = 2;
+    public static final short SERVICE_TYPE_FPA = 4;
+    public static final short SERVICE_TYPE_CHAT = 5;
+    public static final short SERVICE_TYPE_APAAS = 7;
+
+    public String appCert = "";
+    public String appId = "";
     public int expire;
-    public Map<Short, Service> services = new HashMap<>();
+    public int issueTs;
+    public int salt;
+    public Map<Short, Service> services = new TreeMap<>();
 
-    public AccessToken2(String appId, String appCertificate, int expire) {
+    public AccessToken2() {}
+
+    public AccessToken2(String appId, String appCert, int expire) {
+        this.appCert = appCert;
         this.appId = appId;
-        this.appCertificate = appCertificate;
         this.expire = expire;
+        this.issueTs = Utils.getTimestamp();
+        this.salt = Utils.randomInt();
     }
 
     public void addService(Service service) {
-        services.put(service.getServiceType(), service);
+        this.services.put(service.getServiceType(), service);
     }
 
     public String build() throws Exception {
-        int issueTs = (int) (System.currentTimeMillis() / 1000);
-        byte[] signing = generateSigning(issueTs);
-        byte[] content = packContent(issueTs, signing);
-        byte[] signature = encodeHMACSHA256(signing, content);
-        byte[] result = packResult(signature, content);
-        return VERSION_STR + appId + Base64.getEncoder().encodeToString(compress(result));
-    }
-
-    private byte[] generateSigning(int issueTs) throws Exception {
-        byte[] salt = intToBytes(issueTs);
-        byte[] expireBytes = intToBytes(expire);
-        byte[] combined = new byte[salt.length + expireBytes.length];
-        System.arraycopy(salt, 0, combined, 0, salt.length);
-        System.arraycopy(expireBytes, 0, combined, salt.length, expireBytes.length);
-        return encodeHMACSHA256(appCertificate.getBytes(), combined);
-    }
-
-    private byte[] packContent(int issueTs, byte[] signing) throws Exception {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        out.write(intToBytes(issueTs));
-        out.write(intToBytes(expire));
-        out.write(signing);
-        out.write(shortToBytes((short) services.size()));
-        TreeMap<Short, Service> sorted = new TreeMap<>(services);
-        for (Service service : sorted.values()) {
-            out.write(service.pack());
+        if (!Utils.isUUID(this.appId) || !Utils.isUUID(this.appCert)) {
+            return "";
         }
-        return out.toByteArray();
-    }
 
-    private byte[] packResult(byte[] signature, byte[] content) throws Exception {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        out.write(shortToBytes((short) signature.length));
-        out.write(signature);
-        out.write(content);
-        return out.toByteArray();
-    }
+        ByteBuf buf = new ByteBuf().put(this.appId).put(this.issueTs).put(this.expire).put(this.salt).put((short) this.services.size());
+        byte[] signing = getSign();
 
-    private byte[] compress(byte[] data) {
-        Deflater deflater = new Deflater();
-        deflater.setInput(data);
-        deflater.finish();
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        while (!deflater.finished()) {
-            int count = deflater.deflate(buffer);
-            out.write(buffer, 0, count);
-        }
-        return out.toByteArray();
-    }
+        this.services.forEach((k, v) -> {
+            v.pack(buf);
+        });
 
-    public static byte[] encodeHMACSHA256(byte[] key, byte[] data)
-            throws NoSuchAlgorithmException, InvalidKeyException {
         Mac mac = Mac.getInstance("HmacSHA256");
-        mac.init(new SecretKeySpec(key, "HmacSHA256"));
-        return mac.doFinal(data);
+        mac.init(new SecretKeySpec(signing, "HmacSHA256"));
+        byte[] signature = mac.doFinal(buf.asBytes());
+
+        ByteBuf bufferContent = new ByteBuf();
+        bufferContent.put(signature);
+        bufferContent.buffer.put(buf.asBytes());
+
+        return getVersion() + Utils.base64Encode(Utils.compress(bufferContent.asBytes()));
     }
 
-    public static byte[] intToBytes(int value) {
-        return ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(value).array();
+    public Service getService(short serviceType) {
+        if (serviceType == SERVICE_TYPE_RTC) {
+            return new ServiceRtc();
+        }
+        if (serviceType == SERVICE_TYPE_RTM) {
+            return new ServiceRtm();
+        }
+        if (serviceType == SERVICE_TYPE_FPA) {
+            return new ServiceFpa();
+        }
+        if (serviceType == SERVICE_TYPE_CHAT) {
+            return new ServiceChat();
+        }
+        if (serviceType == SERVICE_TYPE_APAAS) {
+            return new ServiceApaas();
+        }
+        throw new IllegalArgumentException(String.format("unknown service type: `%d`", serviceType));
     }
 
-    public static byte[] shortToBytes(short value) {
-        return ByteBuffer.allocate(2).order(ByteOrder.LITTLE_ENDIAN).putShort(value).array();
+    public byte[] getSign() throws Exception {
+        Mac mac = Mac.getInstance("HmacSHA256");
+        mac.init(new SecretKeySpec(new ByteBuf().put(this.issueTs).asBytes(), "HmacSHA256"));
+        byte[] signing = mac.doFinal(this.appCert.getBytes());
+        mac.init(new SecretKeySpec(new ByteBuf().put(this.salt).asBytes(), "HmacSHA256"));
+        return mac.doFinal(signing);
     }
 
-    public abstract static class Service {
-        private short serviceType;
-        protected Map<Short, Integer> privileges = new TreeMap<>();
+    public static String getUidStr(int uid) {
+        if (uid == 0) {
+            return "";
+        }
+        return String.valueOf(uid & 0xFFFFFFFFL);
+    }
+
+    public static String getVersion() {
+        return VERSION;
+    }
+
+    public boolean parse(String token) {
+        if (!getVersion().equals(token.substring(0, Utils.VERSION_LENGTH))) {
+            return false;
+        }
+
+        try {
+            byte[] data = Utils.decompress(Utils.base64Decode(token.substring(Utils.VERSION_LENGTH)));
+            ByteBuf buff = new ByteBuf(data);
+            String signature = buff.readString();
+            this.appId = buff.readString();
+            this.issueTs = buff.readInt();
+            this.expire = buff.readInt();
+            this.salt = buff.readInt();
+            short servicesNum = buff.readShort();
+
+            for (int i = 0; i < servicesNum; i++) {
+                short serviceType = buff.readShort();
+                Service service = getService(serviceType);
+                service.unpack(buff);
+                this.services.put(serviceType, service);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    public static class Service {
+        public short type;
+        public TreeMap<Short, Integer> privileges = new TreeMap<Short, Integer>() {};
+
+        public Service() {}
 
         public Service(short serviceType) {
-            this.serviceType = serviceType;
+            this.type = serviceType;
+        }
+
+        public void addPrivilegeRtc(PrivilegeRtc privilege, int expire) {
+            this.privileges.put(privilege.intValue, expire);
+        }
+
+        public void addPrivilegeRtm(PrivilegeRtm privilege, int expire) {
+            this.privileges.put(privilege.intValue, expire);
+        }
+
+        public void addPrivilegeFpa(PrivilegeFpa privilege, int expire) {
+            this.privileges.put(privilege.intValue, expire);
+        }
+
+        public void addPrivilegeChat(PrivilegeChat privilege, int expire) {
+            this.privileges.put(privilege.intValue, expire);
+        }
+
+        public void addPrivilegeApaas(PrivilegeApaas privilege, int expire) {
+            this.privileges.put(privilege.intValue, expire);
+        }
+
+        public TreeMap<Short, Integer> getPrivileges() {
+            return this.privileges;
         }
 
         public short getServiceType() {
-            return serviceType;
+            return this.type;
         }
 
-        public void addPrivilege(short privilege, int expire) {
-            privileges.put(privilege, expire);
+        public ByteBuf pack(ByteBuf buf) {
+            return buf.put(this.type).putIntMap(this.privileges);
         }
 
-        public byte[] pack() throws Exception {
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            out.write(shortToBytes(serviceType));
-            out.write(packPrivileges());
-            return out.toByteArray();
-        }
-
-        private byte[] packPrivileges() throws Exception {
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            out.write(shortToBytes((short) privileges.size()));
-            for (Map.Entry<Short, Integer> entry : privileges.entrySet()) {
-                out.write(shortToBytes(entry.getKey()));
-                out.write(intToBytes(entry.getValue()));
-            }
-            return out.toByteArray();
+        public void unpack(ByteBuf byteBuf) {
+            this.privileges = byteBuf.readIntMap();
         }
     }
 
     public static class ServiceRtc extends Service {
-        public static final short SERVICE_TYPE = 1;
-        public static final short PRIVILEGE_JOIN_CHANNEL = 1;
-        public static final short PRIVILEGE_PUBLISH_AUDIO_STREAM = 2;
-        public static final short PRIVILEGE_PUBLISH_VIDEO_STREAM = 3;
-        public static final short PRIVILEGE_PUBLISH_DATA_STREAM = 4;
+        public String channelName;
+        public String uid;
 
-        private String channelName;
-        private String uid;
+        public ServiceRtc() {
+            this.type = SERVICE_TYPE_RTC;
+        }
 
         public ServiceRtc(String channelName, String uid) {
-            super(SERVICE_TYPE);
+            this.type = SERVICE_TYPE_RTC;
             this.channelName = channelName;
             this.uid = uid;
         }
 
-        @Override
-        public byte[] pack() throws Exception {
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            out.write(shortToBytes(SERVICE_TYPE));
-            byte[] channelBytes = channelName.getBytes();
-            out.write(shortToBytes((short) channelBytes.length));
-            out.write(channelBytes);
-            byte[] uidBytes = uid.getBytes();
-            out.write(shortToBytes((short) uidBytes.length));
-            out.write(uidBytes);
-            out.write(packPrivilegesPublic());
-            return out.toByteArray();
+        public String getChannelName() {
+            return this.channelName;
         }
 
-        private byte[] packPrivilegesPublic() throws Exception {
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            out.write(shortToBytes((short) privileges.size()));
-            for (Map.Entry<Short, Integer> entry : privileges.entrySet()) {
-                out.write(shortToBytes(entry.getKey()));
-                out.write(intToBytes(entry.getValue()));
-            }
-            return out.toByteArray();
+        public String getUid() {
+            return this.uid;
+        }
+
+        public ByteBuf pack(ByteBuf buf) {
+            return super.pack(buf).put(this.channelName).put(this.uid);
+        }
+
+        public void unpack(ByteBuf byteBuf) {
+            super.unpack(byteBuf);
+            this.channelName = byteBuf.readString();
+            this.uid = byteBuf.readString();
+        }
+    }
+
+    public static class ServiceRtm extends Service {
+        public String userId;
+
+        public ServiceRtm() {
+            this.type = SERVICE_TYPE_RTM;
+        }
+
+        public ServiceRtm(String userId) {
+            this.type = SERVICE_TYPE_RTM;
+            this.userId = userId;
+        }
+
+        public String getUserId() {
+            return this.userId;
+        }
+
+        public ByteBuf pack(ByteBuf buf) {
+            return super.pack(buf).put(this.userId);
+        }
+
+        public void unpack(ByteBuf byteBuf) {
+            super.unpack(byteBuf);
+            this.userId = byteBuf.readString();
+        }
+    }
+
+    public static class ServiceFpa extends Service {
+        public ServiceFpa() {
+            this.type = SERVICE_TYPE_FPA;
+        }
+
+        public ByteBuf pack(ByteBuf buf) {
+            return super.pack(buf);
+        }
+
+        public void unpack(ByteBuf byteBuf) {
+            super.unpack(byteBuf);
+        }
+    }
+
+    public static class ServiceChat extends Service {
+        public String userId;
+
+        public ServiceChat() {
+            this.type = SERVICE_TYPE_CHAT;
+            this.userId = "";
+        }
+
+        public ServiceChat(String userId) {
+            this.type = SERVICE_TYPE_CHAT;
+            this.userId = userId;
+        }
+
+        public String getUserId() {
+            return this.userId;
+        }
+
+        public ByteBuf pack(ByteBuf buf) {
+            return super.pack(buf).put(this.userId);
+        }
+
+        public void unpack(ByteBuf byteBuf) {
+            super.unpack(byteBuf);
+            this.userId = byteBuf.readString();
+        }
+    }
+
+    public static class ServiceApaas extends Service {
+        public String roomUuid;
+        public String userUuid;
+        public Short role;
+
+        public ServiceApaas() {
+            this.type = SERVICE_TYPE_APAAS;
+            this.roomUuid = "";
+            this.userUuid = "";
+            this.role = -1;
+        }
+
+        public ServiceApaas(String roomUuid, String userUuid, Short role) {
+            this.type = SERVICE_TYPE_APAAS;
+            this.roomUuid = roomUuid;
+            this.userUuid = userUuid;
+            this.role = role;
+        }
+
+        public ServiceApaas(String userUuid) {
+            this.type = SERVICE_TYPE_APAAS;
+            this.roomUuid = "";
+            this.userUuid = userUuid;
+            this.role = -1;
+        }
+
+        public String getRoomUuid() {
+            return this.roomUuid;
+        }
+
+        public String getUserUuid() {
+            return this.userUuid;
+        }
+
+        public Short getRole() {
+            return this.role;
+        }
+
+        public ByteBuf pack(ByteBuf buf) {
+            return super.pack(buf).put(this.roomUuid).put(this.userUuid).put(this.role);
+        }
+
+        public void unpack(ByteBuf byteBuf) {
+            super.unpack(byteBuf);
+            this.roomUuid = byteBuf.readString();
+            this.userUuid = byteBuf.readString();
+            this.role = byteBuf.readShort();
         }
     }
 }
--- backend/src/main/java/com/ieum/backend/global/agora/RtcTokenBuilder2.java
+++ backend/src/main/java/com/ieum/backend/global/agora/RtcTokenBuilder2.java
@@ -1,50 +1,39 @@
 package com.ieum.backend.global.agora;
 
 public class RtcTokenBuilder2 {
-
     public enum Role {
         ROLE_PUBLISHER(1),
-        ROLE_SUBSCRIBER(2);
+        ROLE_SUBSCRIBER(2),;
 
-        private final int value;
-        Role(int value) { this.value = value; }
-        public int getValue() { return value; }
-    }
+        public int initValue;
 
-    public String buildTokenWithUid(
-            String appId,
-            String appCertificate,
-            String channelName,
-            int uid,
-            Role role,
-            int tokenExpire,
-            int privilegeExpire) throws Exception {
-
-        return buildTokenWithUserAccount(appId, appCertificate, channelName,
-                uid == 0 ? "" : String.valueOf(uid), role, tokenExpire, privilegeExpire);
-    }
-
-    public String buildTokenWithUserAccount(
-            String appId,
-            String appCertificate,
-            String channelName,
-            String account,
-            Role role,
-            int tokenExpire,
-            int privilegeExpire) throws Exception {
-
-        AccessToken2 token = new AccessToken2(appId, appCertificate, tokenExpire);
-        AccessToken2.ServiceRtc serviceRtc = new AccessToken2.ServiceRtc(channelName, account);
-
-        serviceRtc.addPrivilege(AccessToken2.ServiceRtc.PRIVILEGE_JOIN_CHANNEL, privilegeExpire);
-
-        if (role == Role.ROLE_PUBLISHER) {
-            serviceRtc.addPrivilege(AccessToken2.ServiceRtc.PRIVILEGE_PUBLISH_AUDIO_STREAM, privilegeExpire);
-            serviceRtc.addPrivilege(AccessToken2.ServiceRtc.PRIVILEGE_PUBLISH_VIDEO_STREAM, privilegeExpire);
-            serviceRtc.addPrivilege(AccessToken2.ServiceRtc.PRIVILEGE_PUBLISH_DATA_STREAM, privilegeExpire);
+        Role(int initValue) {
+            this.initValue = initValue;
         }
+    }
 
-        token.addService(serviceRtc);
-        return token.build();
+    public String buildTokenWithUid(String appId, String appCertificate, String channelName, int uid, Role role, int tokenExpire, int privilegeExpire) {
+        return buildTokenWithUserAccount(appId, appCertificate, channelName, AccessToken2.getUidStr(uid), role, tokenExpire, privilegeExpire);
+    }
+
+    public String buildTokenWithUserAccount(String appId, String appCertificate, String channelName, String account, Role role, int tokenExpire,
+            int privilegeExpire) {
+        AccessToken2 accessToken = new AccessToken2(appId, appCertificate, tokenExpire);
+        AccessToken2.Service serviceRtc = new AccessToken2.ServiceRtc(channelName, account);
+
+        serviceRtc.addPrivilegeRtc(AccessToken2.PrivilegeRtc.PRIVILEGE_JOIN_CHANNEL, privilegeExpire);
+        if (role == Role.ROLE_PUBLISHER) {
+            serviceRtc.addPrivilegeRtc(AccessToken2.PrivilegeRtc.PRIVILEGE_PUBLISH_AUDIO_STREAM, privilegeExpire);
+            serviceRtc.addPrivilegeRtc(AccessToken2.PrivilegeRtc.PRIVILEGE_PUBLISH_VIDEO_STREAM, privilegeExpire);
+            serviceRtc.addPrivilegeRtc(AccessToken2.PrivilegeRtc.PRIVILEGE_PUBLISH_DATA_STREAM, privilegeExpire);
+        }
+        accessToken.addService(serviceRtc);
+
+        try {
+            return accessToken.build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 }
